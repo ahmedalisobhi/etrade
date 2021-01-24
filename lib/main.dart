af22f3fb7ba431a2fb49b10449d3d71c6dea832f
Ahmed Sobhi SV <ahmed_sobhi@teacomputers.com>
2021-01-24 12:37:21 +0200

    tr

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      initialRoute: '/login1',
+      initialRoute: '/login',
       routes: {
         // When navigating to the "/" route, build the FirstScreen widget.
         '/login': (context) => LoginScreen(),
